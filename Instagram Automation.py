a612ed5bb0b017932eff6835f81aff9bfa32c864
Joy Victor <69626754+Jhoie@users.noreply.github.com>
2020-09-04 15:58:03 +0100

    Add files via upload
    This version searches for pictures with #greece and likes 5 of them

---
+++ Instagram Automation.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager  # backward compatability
+from selenium.webdriver import ActionChains
+import time
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://www.instagram.com/")
+
+# login
+time.sleep(2)
+driver.find_element_by_name("username").send_keys("hannah91vic")
+driver.find_element_by_name("password").send_keys("08039506623")
+time.sleep(1)
+driver.find_element_by_xpath("//*[@id='loginForm']/div/div[3]").click()
+time.sleep(2)
+
+# opens a hashtag and clicks a post
+driver.get("https://www.instagram.com/explore/tags/greece")
+driver.find_element_by_class_name("v1Nh3").click()
+time.sleep(2)
+
+# loops through several posts of the same hashtag
+i = 0
+while i < 5:
+    time.sleep(1)
+    driver.find_elements_by_class_name("wpO6b")[1].click()
+    time.sleep(1.5)
+    driver.find_element_by_class_name("coreSpriteRightPaginationArrow").click()
+    i+=1
+
+#driver.close()
